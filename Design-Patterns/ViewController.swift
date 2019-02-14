5689aa702cd2117849d397c30e4729db43fd8652
Pavel <pavel.senchenko@gmail.com>
2019-02-14 18:32:00 +0200

    Initial Commit

---
+++ Design-Patterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Design-Patterns
+//
+//  Created by Pavel on 2/14/19.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
